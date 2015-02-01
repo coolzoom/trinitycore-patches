54564515a12f7ee4e5adc312b3dd9936e4aabb2c
LordPsyan <realmsofwarcraft@gmail.com>
2015-02-01 00:54:06 -0600

    Updated 335a_prepatch. removed 434 patches, Eluna patches, and Skyfire patches.

--- Eluna/Lua-Scripts/Example Scripts/example_player_chat.lua
+++
@@ -1,7 +0,0 @@
-local function OnEvents(event, player, msg, Type, lang)
-    if (msg == "asd") then
-        print "asd"
-    end
-end
-
-RegisterPlayerEvent(18, OnEvents)
--- Eluna/Lua-Scripts/Example Scripts/example_player_chat_command.lua
+++
@@ -1,9 +0,0 @@
-local ChatPrefix = "#example"
-
-local function ChatSystem(event, player, msg, _, lang)
-    if (msg:find(ChatPrefix) == 1) then
-        player:SendNotification("Example Chat Command Works")
-    end
-end
-
-RegisterPlayerEvent(18, ChatSystem)
